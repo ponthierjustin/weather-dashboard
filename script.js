b8fe5f6f4c3f4633de0e5992335b7aaca9c8b4a0
Justin <ponthierjustin@gmail.com>
2020-06-17 17:41:03 -0400

    added 5 day forecast and uvindex

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 $(document).ready(function () {
   function searchCityWeather(cityname) {
     var queryURL =
-      "https://api.openweathermap.org/data/2.5/weather?q=" + cityname + "&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityname +
+      "&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
 
     $.ajax({
       url: queryURL,
@@ -11,28 +13,62 @@ $(document).ready(function () {
 
       var CurrentCity = $("<h1>").text(response.name);
       var currentTemp = $("<h3>").text("Temperature: " + response.main.temp);
+
       var currentHumidity = $("<h3>").text(
-        "Humidity: " + response.main.humidity
+        "Humidity: " + response.main.humidity + "%"
       );
       var currentWind = $("<h3>").text(
         "Wind Speed: " + response.wind.speed + " MPH"
       );
+      var grabLat = response.coord.lat;
+      var grabLon = response.coord.lon;
+      var queryURL1 =
+        "https://api.openweathermap.org/data/2.5/onecall?lat=" +
+        grabLat +
+        "&lon=" +
+        grabLon +
+        "&exclude=hourly, minutely&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
 
-      $("#city-info").append(
-        CurrentCity,
-        currentTemp,
-        currentHumidity,
-        currentWind
-      );
+      $.ajax({
+        url: queryURL1,
+        method: "GET",
+      }).then(function (response) {
+        console.log(response);
+
+        var uVindex = $("<h3>").text("UV Index: " + response.current.uvi);
+
+        var dayOne = $("<p>").text("Temperature: " + response.daily[1].temp.day);
+        var dayTwo = $("<p>").text("Temperature: " + response.daily[2].temp.day);
+        var dayThree = $("<p>").text("Temperature: " + response.daily[3].temp.day);
+        var dayFour = $("<p>").text("Temperature: " + response.daily[4].temp.day);
+        var dayFive = $("<p>").text("Temperature: " + response.daily[5].temp.day);
+
+        $("#day-one").append(dayOne);
+        $("#day-two").append(dayTwo);
+        $("#day-three").append(dayThree);
+        $("#day-four").append(dayFour);
+        $("#day-five").append(dayFive);
+        $("#city-info").append(
+          CurrentCity,
+          currentTemp,
+          currentHumidity,
+          currentWind,
+          uVindex
+        );
+      });
     });
   }
+
+  function clear() {
+    $("#city-info").empty();
+    $(".card-text").empty();
+  }
   $("#run-search").on("click", function (event) {
-    
     event.preventDefault();
-    
+    clear();
+
     var inputCity = $("#search-city").val().trim();
 
-    
     searchCityWeather(inputCity);
   });
 });
