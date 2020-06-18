231932261fc404325951412e0b17ec41cb8378de
Justin <ponthierjustin@gmail.com>
2020-06-17 20:41:50 -0400

    small changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     var queryURL =
       "https://api.openweathermap.org/data/2.5/weather?q=" +
       cityname +
-      "&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
+      "&units=imperial&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
 
     $.ajax({
       url: queryURL,
@@ -12,7 +12,7 @@ $(document).ready(function () {
       console.log(response);
 
       var CurrentCity = $("<h1>").text(response.name);
-      var currentTemp = $("<h3>").text("Temperature: " + response.main.temp);
+      var currentTemp = $("<h3>").text("Temperature: " + response.main.temp + "°F");
 
       var currentHumidity = $("<h3>").text(
         "Humidity: " + response.main.humidity + "%"
@@ -27,7 +27,7 @@ $(document).ready(function () {
         grabLat +
         "&lon=" +
         grabLon +
-        "&exclude=hourly, minutely&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
+        "&exclude=hourly, minutely&units=imperial&appid=3f40f6c653ada73b131b1bb93e6ed9c3";
 
       $.ajax({
         url: queryURL1,
@@ -37,11 +37,11 @@ $(document).ready(function () {
 
         var uVindex = $("<h3>").text("UV Index: " + response.current.uvi);
 
-        var dayOne = $("<p>").text("Temperature: " + response.daily[1].temp.day);
-        var dayTwo = $("<p>").text("Temperature: " + response.daily[2].temp.day);
-        var dayThree = $("<p>").text("Temperature: " + response.daily[3].temp.day);
-        var dayFour = $("<p>").text("Temperature: " + response.daily[4].temp.day);
-        var dayFive = $("<p>").text("Temperature: " + response.daily[5].temp.day);
+        var dayOne = $("<p>").text("Temperature: " + response.daily[1].temp.day + "°F");
+        var dayTwo = $("<p>").text("Temperature: " + response.daily[2].temp.day + "°F");
+        var dayThree = $("<p>").text("Temperature: " + response.daily[3].temp.day + "°F");
+        var dayFour = $("<p>").text("Temperature: " + response.daily[4].temp.day + "°F");
+        var dayFive = $("<p>").text("Temperature: " + response.daily[5].temp.day + "°F");
         var dayOneH = $("<p>").text("Humidity: " + response.daily[1].humidity + "%");
         var dayTwoH = $("<p>").text("Humidity: " + response.daily[2].humidity + "%");
         var dayThreeH = $("<p>").text("Humidity: " + response.daily[3].humidity + "%");
